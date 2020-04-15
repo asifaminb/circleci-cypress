c6e2a83025b9209030c9de7525e656e54d95841b
asifamingov <asif.amin@dsiti.qld.gov.au>
2020-04-15 14:05:36 +1000

    initial

--- cypress/integration/presentation/0-start.spec.js
+++ cypress/integration/presentation/0-start.spec.js
@@ -1,6 +1,6 @@
 describe('Visual regression tests', () => {
   it('Should match previous screenshot "about Page"', () => {
-    cy.visit('/transport/registration/register/vehicle/_nocache');
+    cy.visit('/transport/registration/register/vehcle/_nocache');
     cy.matchImageSnapshot();
   });
 });
